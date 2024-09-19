a2410e5a823cc79022d4f54764051bc86965b42f
charleypeng <charleypeng@gmail.com>
2024-09-19 09:58:11 +0800

    typo in summary

--- src/Sharkable/Attributes/SharkEndpointAttribute.cs
+++ src/Sharkable/Attributes/SharkEndpointAttribute.cs
@@ -4,7 +4,7 @@
 /// An api endpoint of Sharkable
 /// </summary>
 /// <param name="group"></param>
-[Obsolete("due to aot incompatablily, SharkEndpoint will update new features in v0.0.5 and above, \n use ISharkEndpoint instead")]
+[Obsolete("due to aot incompatiblily, SharkEndpoint will update new features in v0.0.5 and above, \n use ISharkEndpoint instead")]
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 public sealed class SharkEndpointAttribute(string? group = null) : Attribute
 {
