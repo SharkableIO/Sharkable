568d429c16e30cc7a0fd8a3510bacfe9762391eb
charleypeng <charleypeng@gmail.com>
2024-10-03 23:27:38 +0800

    fix: where results like badrequest should only contains error message

--- src/Sharkable.NativeTest/TestEndpoint.cs
+++ src/Sharkable.NativeTest/TestEndpoint.cs
@@ -7,14 +7,14 @@ public class TestEndpoint : ISharkEndpoint
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("lost", () => Results.Ok("lost and found"));
+        app.MapGet("lost", () => "lost and found".AsBadRequest());
         app.MapPost("lost", ([FromBody]Todo[] todos, [FromServices]ILogger<TestEndpoint> logger) =>
         {
             var opt = Shark.GetOptions<SharkOption>();
             var monitor = Shark.GetService<IMonitor>();
             monitor?.Show();
             logger.LogError(opt?.ApiPrefix);
-            return todos.AsBadRequest();
+            return todos.AsOkResult();
         });
     }
 }
