8e2476576c99e41fb4e4aa5f6d28bb8d3b76bde2
charleypeng <charleypeng@gmail.com>
2024-10-03 12:47:44 +0800

    fix unified result extension method and code refactor

--- src/Sharkable/Reflector/Reflector.cs
+++ src/Sharkable/Reflector/Reflector.cs
@@ -2,7 +2,7 @@ using System.Linq.Expressions;
 
 namespace Sharkable;
 
-public class Reflector
+public partial class Reflector
 {
     internal static IEnumerable<MethodInfo>? GetMethods<T>(T type, 
         BindingFlags flags = BindingFlags.Instance|BindingFlags.Public|BindingFlags.DeclaredOnly, Func<MethodInfo, int, bool>? predicate = null)
@@ -11,22 +11,7 @@ public class Reflector
             type?.GetType().GetMethods(bindingAttr: flags) : 
             type?.GetType().GetMethods(bindingAttr: flags).Where(predicate);
     }
-    /// <summary>
-    /// get an instance of the unified result class object
-    /// </summary>
-    /// <param name="data">data which need to be assigned</param>
-    /// <param name="type">type of the given data</param>
-    /// <returns></returns>
-    internal static object? GetUnifiedResult(object? data, Type type)
-    {
-        var genericType = typeof(UnifiedResult<>);
-        var specificType = genericType.MakeGenericType(type);
-        var instance = Activator.CreateInstance(specificType);
-        var propertyInfo = specificType.GetProperty("Data");
-        propertyInfo?.SetValue(instance, data);
-        return instance;
-    }
-
+    
     internal static Delegate? GetMethodDelegate(MethodInfo methodInfo, object? instance)
     {
         if (instance == null)
