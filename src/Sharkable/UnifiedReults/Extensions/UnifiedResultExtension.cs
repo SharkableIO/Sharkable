f159df52dae541e1ec1248f2e158f04c58451380
charleypeng <charleypeng@gmail.com>
2024-10-03 23:27:38 +0800

    fix: where results like badrequest should only contains error message

--- src/Sharkable/UnifiedReults/Extensions/UnifiedResultExtension.cs
+++ src/Sharkable/UnifiedReults/Extensions/UnifiedResultExtension.cs
@@ -5,6 +5,16 @@ namespace Sharkable;
 
 public static class UnifiedResultExtension
 {
+    /// <summary>
+    /// produce an unified result
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="data"></param>
+    /// <param name="errors"></param>
+    /// <param name="statusCode"></param>
+    /// <param name="extra"></param>
+    /// <param name="timeStamp"></param>
+    /// <returns></returns>
     public static UnifiedResult<TResult>? AsUnifiedResult<TResult>(this TResult? data, 
         string? errors = null, 
         HttpStatusCode statusCode = HttpStatusCode.OK, 
@@ -12,6 +22,23 @@ public static class UnifiedResultExtension
         DateTimeOffset? timeStamp = null)
     {
         return data == null ? null : new UnifiedResult<TResult>(data, errors, statusCode, extra, timeStamp);
+    }
+    /// <summary>
+    /// produce as unified error
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="data"></param>
+    /// <param name="errors"></param>
+    /// <param name="statusCode"></param>
+    /// <param name="extra"></param>
+    /// <param name="timeStamp"></param>
+    /// <returns></returns>
+    public static UnifiedResult<string>? AsUnifiedError(this string? errors, 
+        HttpStatusCode statusCode = HttpStatusCode.OK, 
+        string? extra = null, 
+        DateTimeOffset? timeStamp = null)
+    {
+        return errors == null ? null : new UnifiedResult<string>(null, errors, statusCode, extra, timeStamp);
     }
     
     public static IResult AsOkResult<TResult>(this TResult? data, 
@@ -22,20 +49,18 @@ public static class UnifiedResultExtension
     {
         return data == null ? default! : Results.Ok(data.AsUnifiedResult(errors, statusCode, extra, timeStamp));
     }
-    public static IResult AsBadRequest<TResult>(this TResult? data, 
-        string? errors = null, 
+    public static IResult AsBadRequest(this string? errors, 
         HttpStatusCode statusCode = HttpStatusCode.BadRequest, 
         string? extra = null, 
         DateTimeOffset? timeStamp = null)
     {
-        return data == null ? default! : Results.BadRequest(data.AsUnifiedResult(errors, statusCode, extra, timeStamp));
+        return errors == null ? default! : Results.BadRequest(errors.AsUnifiedError(statusCode, extra, timeStamp));
     }
-    public static IResult AsUnauthorized<TResult>(this TResult? data, 
-        string? errors = null, 
+    public static IResult AsUnauthorized(this string? errors, 
         HttpStatusCode statusCode = HttpStatusCode.Unauthorized, 
         string? extra = null, 
         DateTimeOffset? timeStamp = null)
     {
-        return data == null ? default! : Results.BadRequest(data.AsUnifiedResult(errors, statusCode, extra, timeStamp));
+        return errors == null ? default! : Results.BadRequest(errors.AsUnifiedError(statusCode, extra, timeStamp));
     }
 }
